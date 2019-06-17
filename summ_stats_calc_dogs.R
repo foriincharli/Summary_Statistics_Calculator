762037c18b858ac0822f1eceef05de01a7ef64af
Charli <50045787+foriincharli@users.noreply.github.com>
2019-06-17 14:50:17 +1000

    Add files via upload

---
+++ summ_stats_calc_dogs.R
@@ -0,0 +1,12 @@
+
+
+
+df2 <- df1 %>% 
+  filter(Breed == "French Bulldog") %>% 
+  select(Coat_Colour, Weight, Height, Country_Origin, Size) %>% 
+  group_by(Country_Origin, Size) %>% 
+  summarise_each(funs(mean(., na.rm = T), 
+                      n = sum(!is.na(.)), 
+                      se = sd(., na.rm=T)/sqrt(sum(!is.na(.)))), 
+                 1:5) %>% 
+  mutate_if(is.numeric, round, 2)
