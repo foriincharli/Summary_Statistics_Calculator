d8c307b4cdec14a5892b895e9506a9d34b2ce363
Charli <50045787+foriincharli@users.noreply.github.com>
2019-06-24 12:21:47 +1000

    Update summ_stats.R

--- summ_stats.R
+++ summ_stats.R
@@ -1,6 +1,7 @@
 library(dplyr)
 
-#### Generate some summary statistics ####
+# Summary statistics calculator for 'weather_extra.csv' #
+#### Calculate sample mean, n and standard error of the mean for the columns MinTemp:Rainfall ####
 df2 <- df1 %>% 
   group_by(Year, State, Location) %>% 
   summarise_each(funs(mean(., na.rm = T), 
